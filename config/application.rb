e24376044084be01a9a7f606925e3883907e9b50
Johnny <johnnyin2a@gmail.com>
2013-08-04 12:42:40 -0400

    Configured for deployment to Heroku and added additional tests and test refactoring.

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,9 @@ module SampleApp
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
+    
     # Enable the asset pipeline
     config.assets.enabled = true
 
--- spec/utilities.rb
+++ spec/utilities.rb
@@ -1,8 +1,1 @@
-def full title(page title)
-	base title = "Ruby on Rails Tutorial Sample App" 
-	if page title.empty?
-		base title
-	else
-		"#{base title} | #{page title}" 
-	end
-end
+include ApplicationHelper
